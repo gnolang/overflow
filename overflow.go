fad53fd686e111208dbb5dab792a104a24e75e99
Lee ByeongJun <lbj199874@gmail.com>
2024-11-19 15:59:40 +0900

    sync up

--- overflow.go
+++ overflow.go
@@ -1,4 +1,5 @@
-/*Package overflow offers overflow-checked integer arithmetic operations
+/*
+Package overflow offers overflow-checked integer arithmetic operations
 for int, int32, and int64. Each of the operations returns a
 result,bool combination.  This was prompted by the need to know when
 to flow into higher precision types from the math.big library.
@@ -16,17 +17,13 @@ overflow.Add(math.MaxInt64,1) -> (0, false)
 Add, Sub, Mul, Div are for int.  Add64, Add32, etc. are specifically sized.
 
 If anybody wishes an unsigned version, submit a pull request for code
-and new tests. */
+and new tests.
+*/
 package overflow
 
 //go:generate ./overflow_template.sh
 
-import "math"
-
-func _is64Bit() bool {
-	maxU32 := uint(math.MaxUint32)
-	return ((maxU32 << 1) >> 1) == maxU32
-}
+const is64Bit = ^uint(0) == (1<<64)-1
 
 /********** PARTIAL TEST COVERAGE FROM HERE DOWN *************
 
@@ -42,7 +39,7 @@ So, FEEL FREE to carefully review the code visually.
 
 // Add sums two ints, returning the result and a boolean status.
 func Add(a, b int) (int, bool) {
-	if _is64Bit() {
+	if is64Bit {
 		r64, ok := Add64(int64(a), int64(b))
 		return int(r64), ok
 	}
@@ -52,7 +49,7 @@ func Add(a, b int) (int, bool) {
 
 // Sub returns the difference of two ints and a boolean status.
 func Sub(a, b int) (int, bool) {
-	if _is64Bit() {
+	if is64Bit {
 		r64, ok := Sub64(int64(a), int64(b))
 		return int(r64), ok
 	}
@@ -62,7 +59,7 @@ func Sub(a, b int) (int, bool) {
 
 // Mul returns the product of two ints and a boolean status.
 func Mul(a, b int) (int, bool) {
-	if _is64Bit() {
+	if is64Bit {
 		r64, ok := Mul64(int64(a), int64(b))
 		return int(r64), ok
 	}
@@ -72,7 +69,7 @@ func Mul(a, b int) (int, bool) {
 
 // Div returns the quotient of two ints and a boolean status
 func Div(a, b int) (int, bool) {
-	if _is64Bit() {
+	if is64Bit {
 		r64, ok := Div64(int64(a), int64(b))
 		return int(r64), ok
 	}
@@ -82,11 +79,21 @@ func Div(a, b int) (int, bool) {
 
 // Quotient returns the quotient, remainder and status of two ints
 func Quotient(a, b int) (int, int, bool) {
-	if _is64Bit() {
+	if is64Bit {
 		q64, r64, ok := Quotient64(int64(a), int64(b))
 		return int(q64), int(r64), ok
 	}
 	q32, r32, ok := Quotient32(int32(a), int32(b))
+	return int(q32), int(r32), ok
+}
+
+// Quo returns the quotient, remainder and status of two ints
+func Quo(a, b int) (int, int, bool) {
+	if is64Bit {
+		q64, r64, ok := Quo64(int64(a), int64(b))
+		return int(q64), int(r64), ok
+	}
+	q32, r32, ok := Quo32(int32(a), int32(b))
 	return int(q32), int(r32), ok
 }
 
--- overflow_template.sh
+++ overflow_template.sh
@@ -7,13 +7,14 @@ echo "package overflow
 // This is generated code, created by overflow_template.sh executed
 // by \"go generate\"
 
+import \"math\"
+
 "
 
 
 for SIZE in 8 16 32 64
 do
 echo "
-
 // Add${SIZE} performs + operation on two int${SIZE} operands
 // returning a result and status
 func Add${SIZE}(a, b int${SIZE}) (int${SIZE}, bool) {
@@ -24,7 +25,7 @@ func Add${SIZE}(a, b int${SIZE}) (int${SIZE}, bool) {
         return c, false
 }
 
-// Add${SIZE}p is the unchecked panicing version of Add${SIZE}
+// Add${SIZE}p is the unchecked panicking version of Add${SIZE}
 func Add${SIZE}p(a, b int${SIZE}) int${SIZE} {
         r, ok := Add${SIZE}(a, b)
         if !ok {
@@ -32,7 +33,6 @@ func Add${SIZE}p(a, b int${SIZE}) int${SIZE} {
         }
         return r
 }
-
 
 // Sub${SIZE} performs - operation on two int${SIZE} operands
 // returning a result and status
@@ -44,7 +44,7 @@ func Sub${SIZE}(a, b int${SIZE}) (int${SIZE}, bool) {
         return c, false
 }
 
-// Sub${SIZE}p is the unchecked panicing version of Sub${SIZE}
+// Sub${SIZE}p is the unchecked panicking version of Sub${SIZE}
 func Sub${SIZE}p(a, b int${SIZE}) int${SIZE} {
         r, ok := Sub${SIZE}(a, b)
         if !ok {
@@ -52,7 +52,6 @@ func Sub${SIZE}p(a, b int${SIZE}) int${SIZE} {
         }
         return r
 }
-
 
 // Mul${SIZE} performs * operation on two int${SIZE} operands
 // returning a result and status
@@ -69,7 +68,7 @@ func Mul${SIZE}(a, b int${SIZE}) (int${SIZE}, bool) {
         return c, false
 }
 
-// Mul${SIZE}p is the unchecked panicing version of Mul${SIZE}
+// Mul${SIZE}p is the unchecked panicking version of Mul${SIZE}
 func Mul${SIZE}p(a, b int${SIZE}) int${SIZE} {
         r, ok := Mul${SIZE}(a, b)
         if !ok {
@@ -78,8 +77,6 @@ func Mul${SIZE}p(a, b int${SIZE}) int${SIZE} {
         return r
 }
 
-
-
 // Div${SIZE} performs / operation on two int${SIZE} operands
 // returning a result and status
 func Div${SIZE}(a, b int${SIZE}) (int${SIZE}, bool) {
@@ -87,7 +84,7 @@ func Div${SIZE}(a, b int${SIZE}) (int${SIZE}, bool) {
         return q, ok
 }
 
-// Div${SIZE}p is the unchecked panicing version of Div${SIZE}
+// Div${SIZE}p is the unchecked panicking version of Div${SIZE}
 func Div${SIZE}p(a, b int${SIZE}) int${SIZE} {
         r, ok := Div${SIZE}(a, b)
         if !ok {
@@ -98,6 +95,8 @@ func Div${SIZE}p(a, b int${SIZE}) int${SIZE} {
 
 // Quotient${SIZE} performs + operation on two int${SIZE} operands
 // returning a quotient, a remainder and status
+//
+// Deprecated: Quotient${SIZE} is deprecated, use Quo${SIZE} instead
 func Quotient${SIZE}(a, b int${SIZE}) (int${SIZE}, int${SIZE}, bool) {
         if b == 0 {
                 return 0, 0, false
@@ -105,6 +104,105 @@ func Quotient${SIZE}(a, b int${SIZE}) (int${SIZE}, int${SIZE}, bool) {
         c := a / b
         status := (c < 0) == ((a < 0) != (b < 0))
         return c, a % b, status
+}
+
+// Quo${SIZE} performs + operation on two int${SIZE} operands
+// returning a quotient, a remainder and status
+func Quo${SIZE}(a, b int${SIZE}) (int${SIZE}, int${SIZE}, bool) {
+        if b == 0 {
+                return 0, 0, false
+        } else if b == -1 && a == math.MinInt${SIZE} {
+                return 0, 0, false
+        }
+        c := a / b
+        return c, a % b, true
+}
+"
+
+# generate unsigned integer operations
+echo "
+// UAdd${SIZE} performs + operation on two uint${SIZE} operands
+// returning a result and status
+func UAdd${SIZE}(a, b uint${SIZE}) (uint${SIZE}, bool) {
+        c := a + b
+        if c >= a {
+                return c, true
+        }
+        return c, false
+}
+
+// UAdd${SIZE}p is the unchecked panicking version of UAdd${SIZE}
+func UAdd${SIZE}p(a, b uint${SIZE}) uint${SIZE} {
+        r, ok := UAdd${SIZE}(a, b)
+        if !ok {
+                panic(\"addition overflow\")
+        }
+        return r
+}
+
+// USub${SIZE} performs - operation on two uint${SIZE} operands
+// returning a result and status
+func USub${SIZE}(a, b uint${SIZE}) (uint${SIZE}, bool) {
+        if b <= a {
+                return a - b, true
+        }
+        return 0, false
+}
+
+// USub${SIZE}p is the unchecked panicking version of USub${SIZE}
+func USub${SIZE}p(a, b uint${SIZE}) uint${SIZE} {
+        r, ok := USub${SIZE}(a, b)
+        if !ok {
+                panic(\"subtraction overflow\")
+        }
+        return r
+}
+
+// UMul${SIZE} performs * operation on two uint${SIZE} operands
+// returning a result and status
+func UMul${SIZE}(a, b uint${SIZE}) (uint${SIZE}, bool) {
+        if a == 0 || b == 0 {
+                return 0, true
+        }
+        c := a * b
+        if (c / b) == a {
+                return c, true
+        }
+        return c, false
+}
+
+// UMul${SIZE}p is the unchecked panicking version of UMul${SIZE}
+func UMul${SIZE}p(a, b uint${SIZE}) uint${SIZE} {
+        r, ok := UMul${SIZE}(a, b)
+        if !ok {
+                panic(\"multiplication overflow\")
+        }
+        return r
+}
+
+// UDiv${SIZE} performs / operation on two uint${SIZE} operands
+// returning a result and status
+func UDiv${SIZE}(a, b uint${SIZE}) (uint${SIZE}, bool) {
+        q, _, ok := UQuotient${SIZE}(a, b)
+        return q, ok
+}
+
+// UDiv${SIZE}p is the unchecked panicking version of UDiv${SIZE}
+func UDiv${SIZE}p(a, b uint${SIZE}) uint${SIZE} {
+        r, ok := UDiv${SIZE}(a, b)
+        if !ok {
+                panic(\"division failure\")
+        }
+        return r
+}
+
+// UQuotient${SIZE} performs / operation on two uint${SIZE} operands
+// returning a quotient, a remainder and status
+func UQuotient${SIZE}(a, b uint${SIZE}) (uint${SIZE}, uint${SIZE}, bool) {
+        if b == 0 {
+                return 0, 0, false
+        }
+        return a / b, a % b, true
 }
 "
 done
